0ad838513e3f45a4883d0024cbcb97f87bf100a6
myz666 <2417688748@qq.com>
2018-03-12 16:19:11 +0800

    this is for test in 2018-3-12

---
+++ jxufe_rzsx_test/src/jxufe/rzsx/test/Test.java
@@ -0,0 +1,11 @@
+package jxufe.rzsx.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is a test for jxufe_rzsx");
+		System.out.println("2018-3-12");
+	}
+
+}
