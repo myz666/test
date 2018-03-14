de29ee33b644e859566679d9ebb358251116b802
myz666 <2417688748@qq.com>
2018-03-14 13:30:29 +0800

    this is the 2nd test in March,14,2018

--- jxufe_rzsx_test/src/jxufe/rzsx/test/Test.java
+++ jxufe_rzsx_test/src/jxufe/rzsx/test/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("this is a test for jxufe_rzsx");
 		System.out.println("2018-3-12");
+		System.out.println("2018-3-14");
 	}
 
 }
